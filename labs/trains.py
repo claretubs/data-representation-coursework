ed44bfc1cff4daa322b9c5d3c6a4adf162ff6b68
Clare Tubridy <G00354146@atu.ie>
2023-10-10 17:43:46 +0100

    Lab 2: Trains complete

--- labs/trains.py
+++ labs/trains.py
@@ -5,20 +5,37 @@ import requests
 import csv
 from xml.dom.minidom import parseString
 
+retrieveTags=['TrainStatus',
+            'TrainLatitude',
+            'TrainLongitude',
+            'TrainCode',
+            'TrainDate',
+            'PublicMessage',
+            'Direction'
+            ]
+
 url = 'http://api.irishrail.ie/realtime/realtime.asmx/getCurrentTrainsXML'
 page = requests.get(url)
 doc = parseString(page.content)
 
-# To check it works
-#print(doc.toprettyxml())
 
 # To store the xml in a file
 with open('trainxml.xml', 'w') as xmlfp:
-    doc.writexml(xmlfp)
+   doc.writexml(xmlfp)
 
-obj_train_position_nodes = doc.getElementsByTagName('objTrainPositions')
-for obj_train_position_nodes in obj_train_position_nodes:
-    traincodenode = obj_train_position_nodes.getElementsByTagName('TrainCode').item(0)
-    traincode = traincodenode.firstChild.nodeValue.strip()
-    #print(traincode)
+with open('week02_train.csv', mode='w', newline='') as train_file:
+    train_writer = csv.writer(train_file, delimiter='\t', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+
+    obj_train_position_nodes = doc.getElementsByTagName('objTrainPositions')
+    for obj_train_position_nodes in obj_train_position_nodes:
+        #traincodenode = obj_train_position_nodes.getElementsByTagName('TrainCode').item(0)
+        #traincode = traincodenode.firstChild.nodeValue.strip()
+        
+        # To get all values
+        data_list = []
+        for retrieveTag in retrieveTags:
+            datanode = obj_train_position_nodes.getElementsByTagName(retrieveTag).item(0)
+            data_list.append(datanode.firstChild.nodeValue.strip())
+
+        train_writer.writerow(data_list)
 
